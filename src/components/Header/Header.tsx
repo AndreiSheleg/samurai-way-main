704c6a9a4e52802860d92f62c380f7c985b47f99
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-04-01 23:56:55 +0300

    редактироние css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import s from './Header.module.css'
 function Header () {
     return (
         <header className={s.header}>
-            <img src='https://w7.pngwing.com/pngs/549/766/png-transparent-logo-brand-product-design-font-company-name-logo-with-tag-line-text-logo-antaira-technologies.png' alt={header.foto}/>
+            <img src='https://w7.pngwing.com/pngs/549/766/png-transparent-logo-brand-product-design-font-company-name-logo-with-tag-line-text-logo-antaira-technologies.png' alt='header foto'/>
         </header>
 
     )
