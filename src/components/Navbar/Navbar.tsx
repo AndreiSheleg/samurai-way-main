9507c000afb99321af1b625f8e8a94c9dad45f65
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-04-07 12:51:57 +0300

    урок №19 ROUTE, BrowserRouter , маршрутизация

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,23 @@
 import s from './Navbar.module.css'
-
 // s импортируется их файла модуль css
 
 function Navbar () {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='#s'>Profile</a>
+                <a href='/profile'>Profile</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href='#s'>Messages</a>
+                <a href='/dialogs'>Messages</a>
             </div>
             <div className={s.item}>
-                <a href='#s'>News</a>
+                <a href='/news'>News</a>
             </div>
             <div className={s.item}>
-                <a href='#s'>Music</a>
+                <a href='/music'>Music</a>
             </div>
             <div className={s.item}>
-                <a href='#s'>Settings</a>
+                <a href='/settings'>Settings</a>
             </div>
 
         </nav>
