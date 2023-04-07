ec696d1b9da4267dfaf53fa9ada4cabd1e410880
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-04-07 12:10:44 +0300

    урок №18 добавление компоненты Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 function Profile () {
     return (
-        <div className={s.content}>
+        <div>
             <img
                 src='https://avatars.mds.yandex.net/i?id=6b596b9cb99909263995fe0022478cd10bcac181-4901993-images-thumbs&n=13'/>
             <div>
