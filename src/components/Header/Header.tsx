d76829dd95711db807891e03a828f2bb981bdc20
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-04-30 05:50:48 +0300

    изменено grid-template-rows: 80px 1fr в App.css заменена картинка лого

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import s from './Header.module.css'
+import logo from "../../assets/images/logo.svg"
 
 
 function Header () {
     return (
         <header className={s.header}>
-            <img src='https://w7.pngwing.com/pngs/549/766/png-transparent-logo-brand-product-design-font-company-name-logo-with-tag-line-text-logo-antaira-technologies.png' alt='header foto'/>
+            <img src={logo} alt='logo-img'/>
         </header>
-
     )
 }
 
